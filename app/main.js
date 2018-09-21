1cc45379cc9d965b302fc164ed807f02794ef9ca
weijiyang <crawling_snail@163.com>
2018-09-21 18:24:23 +0800

    add

--- app/main.js
+++ app/main.js
@@ -10,5 +10,6 @@ import React from 'react'
 import {render} from 'react-dom'
 import Greeter from './Greeter'
 import './main.css'
+import './styles.css'
 
 render(<Greeter />,document.getElementById('root'))
